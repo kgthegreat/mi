41cc2197baf54c4ff4b025b77beae745202149da
Kumar Gaurav <kgthegreat@gmail.com>
2012-04-18 19:47:20 +0530

    Added required gems, integrated specs, specified mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mi::Application.config.secret_token = 'c7b495a8d38d46b11da77214437cf85435a1577ef8af8c627b21f8add30ce50c42bc239a23d4bf6b6babdabf53324eb8f89b20b52ff3f94362b2d4d0ca9588e2'
