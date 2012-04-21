58b545f4c6284650446f5942972f5fa035a84c4b
Kumar Gaurav <kgthegreat@gmail.com>
2012-04-21 12:37:29 +0530

    Added edit and index

--- app/controllers/admin/domains_controller.rb
+++ app/controllers/admin/domains_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DomainsController < ApplicationController
 
   respond_to :html
+
+  before_filter :find_domain, :only => [:edit,:update]
   
   def new
     @domain = Domain.new
@@ -11,7 +13,30 @@ class Admin::DomainsController < ApplicationController
     if @domain.save
       respond_with @domain, :location => admin_domains_path
     else
-      respond_with @domain
+      render :new
+
     end
+  end
+
+  def index
+    respond_with @domains = Domain.all
+  end
+
+  def edit
+    respond_with @domain
+  end
+
+  def update
+    if @domain.update_attributes(params[:domain])
+      respond_with @domain, :location => admin_domains_path
+    else
+      render :edit
+    end  
+  end
+
+  private
+
+  def find_domain
+    @domain = Domain.find params[:id]
   end
 end
--- spec/controllers/admin/domains_controller_spec.rb
+++ spec/controllers/admin/domains_controller_spec.rb
@@ -4,7 +4,9 @@ describe Admin::DomainsController do
 
   render_views
 
-
+  before :each do
+    @domain = Domain.create!({:name=>"somename"})
+  end
   describe "GET new" do
     it "should render the new template" do
       get :new
@@ -27,8 +29,47 @@ describe Admin::DomainsController do
 
     it "should not create a new domain if the name is missing" do
       post :create, :domain => {:name => ""}
-      redirect_to new_admin_domain_path
+      response.should render_template :new
+    end
+  end
+
+  describe "GET index" do
+    it "should list all the domains" do
+      get :index
+      assigns[:domains].should eq Domain.all
     end
 
+    it "should render index template" do
+      get :index
+      response.should render_template :index
+    end
+  end
+
+  describe "GET edit" do
+    before :each do
+
+      get :edit, :id => @domain
+    end
+    it "should render edit template" do
+      response.should render_template :edit
+    end
+    it "should assign @domain with the domain to be edited" do
+      assigns[:domain].should eq @domain
+    end
+  end
+
+  describe "PUT update" do
+    before :each do
+      post :update, :id => @domain, :domain => {:name=> "someother"}      
+    end
+
+    it "should update the domain in question" do
+      
+      @domain.reload.name.should eq "someother"
+    end
+    it "should render edit template if update fails" do
+      post :update, :id => @domain, :domain => {:name=> ""}
+      response.should render_template :edit
+    end
   end
 end
