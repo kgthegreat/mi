3288be615ba22f105eeeec9dba0019be27a590e7
Kumar Gaurav <kgthegreat@gmail.com>
2012-04-23 15:44:27 +0530

    Added the ability to add and delete trainers for an admin

--- app/controllers/admin/trainers_controller.rb
+++ app/controllers/admin/trainers_controller.rb
@@ -1,10 +1,31 @@
 class Admin::TrainersController < ApplicationController
   respond_to :html
-  
+
+  before_filter :authenticate_admin!
   def create
     @trainer = Trainer.new(params[:trainer])
     if @trainer.save
       respond_with @trainer, :location => admin_trainers_path
+    else
+      render :new
     end
-  end  
+    
+  end
+
+  def index
+    respond_with @trainers = Trainer.all
+  end
+
+  def new
+    respond_with @trainer = Trainer.new
+  end
+
+  def destroy
+    @domain = Trainer.find params[:id]
+    @domain.destroy
+    flash[:notice] = "Entry deleted"
+    redirect_to admin_trainers_path
+  end
+  
 end
+
--- spec/controllers/admin/trainers_controller_spec.rb
+++ spec/controllers/admin/trainers_controller_spec.rb
@@ -3,21 +3,114 @@ require 'spec_helper'
 describe Admin::TrainersController do
   render_views
 
+
+  before :each do
+    @admin_user = Admin.create!({:email=>"admin@admin.com", :password => "asd123"})
+    sign_in @admin_user
+    @trainer = Trainer.create!(:email => "hello@hello.com", :password => "password")
+
+  end
+
   describe "GET index" do
-    it "should render index template"
-    it "should show a list of trainers"
+
+    before :each do
+      get :index
+    end
+    it "should render index template" do
+      response.should render_template :index
+    end
+    it "should show a list of trainers" do
+      assigns[:trainers].should eq Trainer.all
+    end
+    context "failure" do
+      context "for normal user" do
+        before :each do
+          sign_out @admin_user
+          @user = User.create!(:email => "hello@hello.com", :password => "password")
+          sign_in @user
+          get :index
+        end
+        it "should not be accessible to non admin user" do
+          response.should_not render_template :index
+        end
+        it "should be a redirect" do
+          response.should be_redirect
+        end
+      end
+      context "for trainers" do
+        before :each do
+          sign_out @admin_user
+          sign_in @trainer
+          get :index
+        end
+        it "should not be accessible to non admin user" do
+          response.should_not render_template :index
+        end
+        it "should be a redirect" do
+          response.should be_redirect
+        end
+      end
+    end
   end
 
   describe "GET new" do
-    it "should render new template"
+    before :each do
+      get :new
+    end
+    it "should render new template" do
+      response.should render_template :new
+    end
   end
 
   describe "POST create" do
     it "should create a new trainer" do
       lambda { post :create, :trainer => {:email => "some@some.com",:password => "asd123"}
       }.should change(Trainer, :count).by(1)
-      
+    end
+
+    it "should not create a trainer without an email" do
+      lambda { post :create, :trainer => {:email => "",:password => "asd123"}
+      }.should change(Trainer, :count).by(0)
+
+    end
+
+    it "should not create a trainer without a password" do
+      lambda { post :create, :trainer => {:email => "some@some",:password => ""}
+      }.should change(Trainer, :count).by(0)
+
+    end
+
+    it "should not create a trainer without a valid format of emails" do
+      lambda { post :create, :trainer => {:email => "some",:password => "asd123"}
+      }.should change(Trainer, :count).by(0)
+
+    end
+
+    it "should render new template in case create is not successful" do
+      post :create, :trainer => {:email => "some",:password => "asd123"}
+      response.should render_template :new
+    end
+    
+  end
+
+
+  describe "DELETE destroy" do
+    it "should remove the trainer" do
+      lambda {
+        delete :destroy, :id => @trainer
+      }.should change(Trainer, :count).by(-1)
       
     end
+
+    it "should inform the user of the deletion" do
+      delete :destroy, :id => @trainer
+      flash[:notice].should include "Entry deleted"
+    end
+
+    it "should redirect to list of domains" do
+      delete :destroy, :id => @trainer
+      response.should redirect_to admin_trainers_path
+    end
   end
+
 end
