46c8e6a1466e901c480658c8959f95e59302d903
Kumar Gaurav <kgthegreat@gmail.com>
2012-04-28 17:32:44 +0530

    Added homepage and layout. Added faq

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Mi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
@@ -62,8 +62,10 @@ Mi::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
+  root :to => 'home#index'
   namespace :admin do
     resources :domains
     resources :trainers
+    resources :faqs
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423014959) do
+ActiveRecord::Schema.define(:version => 20120428050019) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(:version => 20120423014959) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "faqs", :force => true do |t|
+    t.text     "question"
+    t.text     "answer"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "trainers", :force => true do |t|
