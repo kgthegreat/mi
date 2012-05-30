59066396c54a2a565a261ef73dcbe4b62507691a
Kumar Gaurav <kgthegreat@gmail.com>
2012-05-30 06:36:41 +0530

    Association tests on schedule

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe Schedule do
-  it { should belong_to :user }
-  it { should belong_to :domain }
+
+  context "associations" do
+    it { should belong_to :user }
+    it { should belong_to :domain }
+  end
+
+  context "validations" do
+    it {should validate_presence_of :domain_id}
+  end
 end
