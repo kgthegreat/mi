b84fc79989d9134f7a351baeed6b43653dc666af
Kumar Gaurav <kgthegreat@gmail.com>
2012-06-14 07:40:32 +0530

    Scheduling

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,8 +6,9 @@ class SchedulesController < HomeController
 
   def create
     @schedule = Schedule.new params[:schedule]
+    @schedule.timeslot = Timeslot.find_by_date_and_start_time(params[:timeslot_date], params[:timeslot_time])
     if @schedule.save
-      respond_with @schedule, :location => schedules_path
+      respond_with @schedule, :location => '/'
     else
       respond_with @schedule
     end
